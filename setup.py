8ac9724bfcd5a15f888407fd34d744973c0bcdc9
GoodiesHQ <goodies@protonmail.com>
2017-11-17 11:05:13 -0800

    Version change

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 
 setup(
     name="pypad",
-    version="2.0.1",
-    author="Austin Archer",
+    version="2.0.2",
+    author="Austin Archer",k
     author_email="goodies@protonmail.com",
     license="GPL3.0",
     keywords="cipher block pad padding iso10126 ansix923 x923 pkcs7",
